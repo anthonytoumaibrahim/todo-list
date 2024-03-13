9411fa2a11da211615a207392d3dfa2c60d703b5
atian155 <atianthony@outlook.com>
2024-03-13 19:59:40 +0200

    Backend: added login function

---
+++ backend/login.php
@@ -0,0 +1,48 @@
+<?php
+require_once('connection.php');
+
+$usernameOrEmail = $_POST['username'] ?? "";
+$password = $_POST['password'] ?? "";
+
+function login($conn, $usernameOrEmail, $password)
+{
+  $response = [
+    'status' => false,
+    'message' => '',
+    'data' => [],
+  ];
+
+  // Validation
+  if (empty($usernameOrEmail) || empty($password)) {
+    $response['message'] = "Username/Email or Password cannot be empty.";
+    return json_encode($response);
+  }
+
+  // Check if user exists
+  $query = $conn->prepare("SELECT id, username, email, password FROM users WHERE username=? OR email=?");
+  $query->bind_param('ss', $usernameOrEmail, $usernameOrEmail);
+  $query->execute();
+  $query->store_result();
+  $query->bind_result($id, $username, $email, $hashed_password);
+  $query->fetch();
+  $num_rows = $query->num_rows();
+
+  if ($num_rows == 0) {
+    $response['message'] = "User does not exist.";
+    return json_encode($response);
+  }
+
+  // Check if password is not correct
+  if (!password_verify($password, $hashed_password)) {
+    $response['message'] = "Password is incorrect.";
+    return json_encode($response);
+  }
+
+  // Login successful
+  $response['status'] = true;
+  $response['message'] = "Login successful.";
+  $response['data']['user_id'] = $id;
+  return json_encode($response);
+}
+
+echo login($conn, $usernameOrEmail, $password);
