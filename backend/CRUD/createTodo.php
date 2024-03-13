21dd4effcac83cda29e4b951ade9271a83a4f627
atian155 <atianthony@outlook.com>
2024-03-13 23:04:28 +0200

    Added CRUD operations

---
+++ backend/CRUD/createTodo.php
@@ -0,0 +1,32 @@
+<?php
+require_once('../connection.php');
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+$userId = $data['userId'] ?? 0;
+$value = $data['value'] ?? "";
+
+$response = [
+  'status' => false,
+  'message' => '',
+  'data' => []
+];
+
+if ($userId === 0) {
+  $response['message'] = "User ID is required";
+  exit(json_encode($response));
+}
+
+$checked = false;
+$important = false;
+
+$create = $conn->prepare("INSERT INTO todos (value, checked, important, user_id) VALUES (?, ?, ?, ?)");
+$create->bind_param('siis', $value, $checked, $important, $userId);
+$create->execute();
+
+$response['status'] = true;
+$response['message'] = "Todo created successfully";
+$response['data']['id'] = $conn->insert_id;
+
+echo json_encode($response);
---
+++ backend/CRUD/getTodos.php
@@ -0,0 +1,39 @@
+<?php
+require_once('../connection.php');
+
+$userId = $_GET['userId'] ?? 0;
+
+$response = [
+  'status' => false,
+  'message' => '',
+  'data' => []
+];
+
+if ($userId === 0) {
+  $response['message'] = "User ID is required";
+  exit(json_encode($response));
+}
+
+$todos_query = $conn->prepare("SELECT id, value, checked, important FROM todos WHERE user_id=?");
+$todos_query->bind_param('i', $userId);
+$todos_query->execute();
+$todos_query->store_result();
+$todos_query->bind_result($id, $value, $checked, $important);
+
+$todos = [];
+
+while ($todos_query->fetch()) {
+  $todo = [
+    'id' => $id,
+    'value' => $value,
+    'checked' => $checked,
+    'important' => $important
+  ];
+  $todos[] = $todo;
+}
+
+$response['status'] = true;
+$response['message'] = "Todos fetched successfully";
+$response['data'] = $todos;
+
+echo json_encode($response);
